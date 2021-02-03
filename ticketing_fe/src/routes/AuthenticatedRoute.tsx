43033ada26c140d4939c58f8ca86c2d4d66a88c9
Benas <benasmiliunas@gmail.com>
2021-02-03 20:33:39 +0200

    Created AuthContext

--- ticketing_fe/src/routes/AuthenticatedRoute.tsx
+++ ticketing_fe/src/routes/AuthenticatedRoute.tsx
@@ -1,13 +1,16 @@
 import { Route, Redirect } from 'react-router-dom';
-import React from 'react';
+import React, { useContext } from 'react';
+import { getCookie } from '../utils/cookieService';
 
 interface ProtectedRouteI {
   path: string;
 }
 
 const AuthenticatedRoute: React.FC<ProtectedRouteI> = ({ path, children, ...rest }) => {
+  const token = getCookie('jwt');
+  console.log('token', token);
   // eslint-disable-next-line no-constant-condition
-  return <Route {...rest} render={(routeProps: any) => (false ? children : <Redirect to={'/login'} />)} />;
+  return <Route {...rest} render={(routeProps: any) => (token !== '' ? children : <Redirect to={'/login'} />)} />;
 };
 
 export default AuthenticatedRoute;
